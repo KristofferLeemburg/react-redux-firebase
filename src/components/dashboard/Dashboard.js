e492569526b028b1734ca1894388f933ba604808
leemburg <kristofferleemburg@gmail.com>
2021-03-13 01:55:28 +0100

    add mockup post

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,19 +1,22 @@
+import React, { Component } from 'react'
+import ProjectList from '../projects/ProjectList'
+import Notifications from './Notifications'
 
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SignedInLinks from './SignedInLinks'
-import SignedOutLinks from './SignedOutLinks'
-
-const Navbar = () => {
-  return (
-    <nav className="nav-wrapper grey darken-3">
-      <div className="container">
-        <Link to='/' className="brand-logo">MarioPlan</Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="dashboard container">
+        <div className="row">
+          <div className="col s12 m6">
+            <ProjectList />
+          </div>
+          <div className="col s12 m5 offset-m1">
+            <Notifications />
+          </div>
+        </div>
       </div>
-    </nav>
-  )
+    )
+  }
 }
 
-export default Navbar
+export default Dashboard
